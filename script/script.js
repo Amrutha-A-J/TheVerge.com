c9db020f61e784ed4403e6fa730a15b33b59a4a6
Amrutha-A-J <amruthaajadiyath@gmail.com>
2018-05-24 11:09:50 +0530

    finished fully responsive navbar

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 function DD_click(n){
-    const trigger=document.getElementsByClassName("navBar__visible");
-    for(i=0;i<trigger.length;i++){
-        trigger[i].style.backgroundColor="#000";
-    }
-    trigger[n-1].style.backgroundColor="#1a1a1a";
+    // const trigger=document.getElementsByClassName("navBar__visible");
+    // trigger[n-1].style.backgroundColor=(trigger[n-1].style.backgroundColor==="#1a1a1a"?"#000":"#1a1a1a");
+    // for(i=0;i<trigger.length;i++){
+    //     trigger[i].style.backgroundColor="#000";
+    // }
     const target=document.getElementById("DD"+n);
     target.style.display=(target.style.display==="block"?"none":"block");
     const siblings = target.parentNode.children;
